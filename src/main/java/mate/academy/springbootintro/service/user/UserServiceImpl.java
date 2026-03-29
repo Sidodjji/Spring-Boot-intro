bc06b5a5090d4bd74bf6f8d156755ca9608e2b06
Andrii <andreymaluk35@gmail.com>
2026-03-29 20:16:04 +0300

    Added USER role in registration

--- src/main/java/mate/academy/springbootintro/service/user/UserServiceImpl.java
+++ src/main/java/mate/academy/springbootintro/service/user/UserServiceImpl.java
@@ -1,11 +1,14 @@
 package mate.academy.springbootintro.service.user;
 
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import mate.academy.springbootintro.dto.user.UserRegistrationRequestDto;
 import mate.academy.springbootintro.dto.user.UserResponseDto;
 import mate.academy.springbootintro.exeption.RegistrationException;
 import mate.academy.springbootintro.mapper.UserMapper;
+import mate.academy.springbootintro.model.Role;
 import mate.academy.springbootintro.model.User;
+import mate.academy.springbootintro.repository.RoleRepository;
 import mate.academy.springbootintro.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
@@ -17,12 +20,17 @@ public class UserServiceImpl implements UserService {
 
     private final UserMapper userMapper;
 
+    private final RoleRepository roleRepository;
+
     @Override
     public UserResponseDto register(UserRegistrationRequestDto requestDto) {
         if (userRepository.findByEmail(requestDto.getEmail()).isPresent()) {
             throw new RegistrationException("Can't register user");
         }
         User user = userMapper.toModel(requestDto);
+        Role userRole = roleRepository.findByRole(Role.RoleName.USER).orElseThrow(
+                () -> new RuntimeException("Role USER not found"));
+        user.setRoles(Set.of(userRole));
         return userMapper.toDto(userRepository.save(user));
     }
 }
