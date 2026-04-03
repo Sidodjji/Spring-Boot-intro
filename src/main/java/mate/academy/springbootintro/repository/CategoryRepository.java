ec69b0b0b8b1efeea76402f4f809ed0ad62332eb
Andrii <andreymaluk35@gmail.com>
2026-04-03 22:53:28 +0300

    Fix issues from comments

--- src/main/java/mate/academy/springbootintro/repository/CategoryRepository.java
+++ src/main/java/mate/academy/springbootintro/repository/CategoryRepository.java
@@ -4,6 +4,5 @@ import mate.academy.springbootintro.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
